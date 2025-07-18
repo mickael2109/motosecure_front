e94c58efbc5d7bd69f94aed7ed7a4205103ad83c
mickael <mickaelrkt20@gmail.com>
2025-07-18 15:16:22 +0300

    design first page

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,4 +1,5 @@
 import fond from "../../assets/fond/fond.svg"
+import { GiFullMotorcycleHelmet } from "react-icons/gi";
 
 const Auth = () => {
     return (
@@ -7,11 +8,14 @@ const Auth = () => {
             <img 
                 src={fond} 
                 alt="fond" 
-                className="absolute top-0 left-0 w-full h-full object-cover blur-[2px] -z-10" 
+                className="absolute top-0 left-0 w-full h-full object-cover blur-[1px] -z-10" 
             />
 
             {/* Contenu par-dessus */}
-            <span className=" text-4xl font-bold z-10 opacity-80 text-white_mc">MotoSecure MG</span>
+            <div className=" text-2xl text-[#6cf7d8] font-bold z-10 opacity-80 flex flex-col items-center gap-4">
+                <i className="text-6xl "><GiFullMotorcycleHelmet /></i>
+                <span>MotoSecure MG</span>
+            </div>
         </div>
 
     );
