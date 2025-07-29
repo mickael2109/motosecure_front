d9417d3f332428b811025a9f4ef23cd65f871bc8
mickael <mickaelrkt20@gmail.com>
2025-07-29 22:03:56 +0300

    test login 2

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -11,18 +11,18 @@ const Login = () => {
             <img 
                 src={fond} 
                 alt="fond" 
-                className="absolute top-0 left-0 w-full h-full object-cover -z-10 brightness-80 opacity-80" 
+                className="absolute top-0 left-0 w-full h-full object-cover -z-10 brightness-80 opacity-20 dark:opacity-80" 
             />
 
             {/* Contenu par-dessus */}
-            <div className=" text-2xl z-10 opacity-80 flex flex-col items-end justify-end gap-4 bg-gradient-to-b from-transparent via-black dark:via-black/40 to-black  w-full h-screen backdrop-blur-[3px]">
-                <div className="flex flex-col gap-2 h-[50vh] w-full p-4">
-                    <div><span className="font-extrabold text-4xl">Login</span></div>
-                    <form className="flex flex-col gap-8">
+            <div className=" text-2xl z-10 opacity-80 flex flex-col items-end justify-end gap-4 bg-gradient-to-b from-transparent  dark:via-black/40  to-second_mc dark:to-black  w-full h-screen">
+                <div className="flex flex-col gap-2 h-[50vh] w-full p-4 ">
+                    <div><span className="font-extrabold text-4xl dark:text-white text-black">Login</span></div>
+                    <form className="flex flex-col gap-8 ">
                         <div className="flex flex-col gap-2">
                             <div><input className="input validator w-full" type="email" required placeholder="Email" /></div>
                             <div><input className="input validator w-full" type="password" required placeholder="Mot de passe" /></div>
-                            <div><button type="submit" className="w-full bg-second_mc p-4 text-[14px] cursor-pointer">Login</button></div>
+                            <div><button type="submit" className="w-full bg-black text-white dark:bg-second_mc p-4 text-[14px] cursor-pointer ">Login</button></div>
                         </div>
                         <div className="text-center flex flex-col gap-2">
                             <div><span className="text-[12px]">- Or sign with -</span></div>
